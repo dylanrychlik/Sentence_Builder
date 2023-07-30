9d9bc8a719fc7447a46ad1e5d5198dad6372118a
Dylan Rychlik <dylanrychlik@gmail.com>
2023-07-30 17:55:49 -0400

    Need Kurt to take a look

--- app/src/main/java/com/example/sentencebuilder/MainActivity.kt
+++ app/src/main/java/com/example/sentencebuilder/MainActivity.kt
@@ -1,22 +1,37 @@
 package com.example.sentencebuilder
 
+import android.Manifest.permission.RECORD_AUDIO
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
 
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.runBlocking
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.io.File
+import java.io.IOException
 
 
 class MainActivity : FragmentActivity() {
     private val fragment = WordFragment()
     private var currentStep = 1
-
+    private var recorder: MediaRecorder? = null
+    private var outputFilePath: String? = null
+    private val RECORD_AUDIO_PERMISSION_CODE = 101
     private val wordViewModel: WordViewModel by viewModels()
     private val WordViewModelSelectedImage: WordViewModel by viewModels()
     private val PICK_IMAGE_REQUEST = 1
+    private var uri: Uri? = null // Initialize uri as nullable Uri
     companion object {
         const val WIZARD_ACTIVITY_REQUEST_CODE = 123 // You can use any integer value
     }
@@ -24,88 +39,141 @@ class MainActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       // displaySelectedWordFragment()
         displayWordFragment()
-        val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        // adapter = lnoun
+        val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener {
-           initializeAddButton()
+            initializeAddButton()
         }
 
         val build_sentence = findViewById<Button>(R.id.build_sentence)
         build_sentence.setOnClickListener {
             showNextStep()
-           println("Drych the legend " + listOf(WordViewModelSelectedImage.wordList).size)
-
+            println("Drych the legend " + listOf(WordViewModelSelectedImage.wordList).size)
         }
-
     }
 
     private fun showNextStep() {
-
-
         val intent = Intent(this, WizardActivity::class.java)
         startActivityForResult(intent, WIZARD_ACTIVITY_REQUEST_CODE)
-
-
     }
 
-
-   private fun displaySelectedWordFragment(){
-
-    }
     private fun displayWordFragment() {
         supportFragmentManager.beginTransaction()
             .add(R.id.word_fragment, fragment, "WordFragment")
             .commit()
     }
 
-
     private fun initializeAddButton() {
-        //WordUri("fish",0,Uri.parse("file:///sdcard/Download/image.jpg"))
-        wordViewModel.addWord("fish",R.drawable.image)
-//        val intent = Intent(Intent.ACTION_GET_CONTENT)
-//        intent.type = "image/*"
-//        startActivityForResult(intent, PICK_IMAGE_REQUEST)
-    }
-     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+        // Check if the uri is not null, meaning an image is already picked
+        if (uri != null) {
+            startRecordingWithPermission()
+        } else {
+            checkRecordAudioPermission()
+        }
 
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
-            data?.data?.let { imageUri ->
-                wordViewModel.addWord("fish", imageUri,null)
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    private fun checkRecordAudioPermission() {
+        if (ContextCompat.checkSelfPermission(this, RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(RECORD_AUDIO),
+                RECORD_AUDIO_PERMISSION_CODE
+            )
+        } else {
+            // Permission has already been granted, you can start recording here.
+            startRecordingWithPermission()
+        }
+    }
+
+    private fun startRecordingWithPermission() {
+        if (uri == null) {
+            // If uri is still null, that means an image is not picked yet, so don't start recording
+            return
+        }
+
+        // Start recording and set the URI once it's done
+        startRecording()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == RECORD_AUDIO_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, start recording.
+                startRecording()
+            } else {
+                // Permission denied, handle this case (e.g., show a message or disable recording).
             }
         }
     }
 
-  /*  private fun handleActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
-            data?.data?.let { imageUri ->
-                // Use the imageUri as desired
-                // For example, you can display it in an ImageView or perform further operations
-
-            }
-        }
-    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
             data?.data?.let { imageUri ->
-                // Use the imageUri as desired
-                // For example, you can display it in an ImageView or perform further operations
-                handleActivityResult(requestCode, resultCode, data)
+                // Check if the recording is done (uri is not null)
+
+                    // Start recording and set the URI once it's done
+                uri = imageUri
+                // Start recording
+                //startRecording()
+                    wordViewModel.addWord("fish", imageUri, uri!!)
+                }
             }
         }
+
+
+
+    private fun startRecording() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request audio recording permission if not granted
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(RECORD_AUDIO),
+                RECORD_AUDIO_PERMISSION_CODE
+            )
+        } else {
+            // Permission has already been granted, start recording
+            try {
+                outputFilePath =
+                    "${Environment.getExternalStorageDirectory().absolutePath}/recording.3gp"
+                recorder = MediaRecorder()
+                recorder?.apply {
+                    setAudioSource(MediaRecorder.AudioSource.MIC)
+                    setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+                    setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+                    setOutputFile(outputFilePath)
+                    prepare()
+                    start()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
-    }*/
-
-
-
-
-
     }
 
+    private fun stopRecording() {
+        recorder?.apply {
+            stop()
+            release()
+        }
+    }
+}
 
